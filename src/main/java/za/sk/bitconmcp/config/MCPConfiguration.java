9ff7e470b05252be7fa4482377303653acd59da1
Andre Kapp <kappaj@gmail.com>
2025-08-24 18:18:47 +0200

    Still testing

--- src/main/java/za/sk/bitconmcp/config/MCPConfiguration.java
+++ src/main/java/za/sk/bitconmcp/config/MCPConfiguration.java
@@ -1,18 +1,24 @@
 package za.sk.bitconmcp.config;
 
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import za.sk.bitconmcp.service.BitcoinServiceClient;
 
+import java.util.List;
+
 @Configuration
 public class MCPConfiguration {
 
-     @Bean
-     public ToolCallbackProvider bitCoinTools(BitcoinServiceClient bitcoinServiceClient){
-          return MethodToolCallbackProvider.builder()
-                  .toolObjects(bitcoinServiceClient)
-                  .build();
-     }
+    //     @Bean
+//     public ToolCallbackProvider bitCoinTools(BitcoinServiceClient bitcoinServiceClient){
+//          return MethodToolCallbackProvider.builder()
+//                  .toolObjects(bitcoinServiceClient)
+//                  .build();
+//     }
+    @Bean
+    public List<ToolCallback> bitcointToolsToolCallback(BitcoinServiceClient bitcoinServiceClient) {
+        return List.of(ToolCallbacks.from(bitcoinServiceClient));
+    }
 }
